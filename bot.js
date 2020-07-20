3ff2a84071a78a56637efa78163ca01ede319153
harithh775 <62695891+harithh775@users.noreply.github.com>
2020-07-20 15:49:10 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -160,13 +160,13 @@ bot.on("guildMemberAdd", async member => {
            .toBuffer();
       }
       let Channelz = member.guild.channels.get("734683523704815737");
-      Channelz.send(`***Wilujeng Rawuh Ngeh*** **${member.user}
+      Channelz.send(`***Wilujeng Rawuh Ngeh ${member.user}
 > Take role <#667009218380824594>
 > Kenalken <#666954959593668608>
 > Mading <#681117738629988389>
 > Kamus <#700850650581303366>
 Matur suwun, enjoy ya disini
-**`,{
+***`,{
         
     files: [{
       
@@ -242,7 +242,7 @@ bot.on("guildMemberRemove", async member => {
 bot.on("guildMemberAdd", async member => {
 
       let BG = db.get(`${member.guild.id}.Config.Welcome.Background`);
-      if (!BG) BG = "https://cdn.discordapp.com/attachments/733961963771527169/734672045652181102/122323423423424.jpg";
+      if (!BG) BG = "https://cdn.discordapp.com/attachments/733961963771527169/734692144513810442/23424234234234234.jpg";
 
       let MSG = db.get(`${member.guild.id}.Config.Welcome.Message`);
       if (!MSG) MSG = "selamat datang bro";
@@ -295,12 +295,70 @@ bot.on("guildMemberAdd", async member => {
 
 });
 
+
+bot.on("guildMemberRemove", async member => {
+
+      let BG = db.get(`${member.guild.id}.Config.Welcome.Background`);
+      if (!BG) BG = "https://cdn.discordapp.com/attachments/733961963771527169/734692142563196998/123123123123123123123.jpg";
+
+      let MSG = db.get(`${member.guild.id}.Config.Welcome.Message`);
+      if (!MSG) MSG = "selamat datang bro";
+
+      var imageUrlRegex = /\?size=1048$/g;
+      var { body: avatar } = await get(
+        member.user.displayAvatarURL.replace(imageUrlRegex, "?size1048")
+      );
+      var { body: background } = await get(`${BG}`);
+      async function createCanvas() {
+          return new Canvas(1024, 500)
+           .addImage(background, 0, 0, 1024, 500)
+           .setColor("#1F97CB")
+           .addRect(-774, 0, 1024, 500)
+           .setColor("#1F97CB")
+           .setShadowColor("rgba(22, 22, 22, 1)") // This is a nice colour for a shadow.
+           .setShadowOffsetY(5) // Drop the shadow by 5 pixels.
+           .setShadowBlur(10) 
+           .addRect(500, 80, 1024, 120)
+           .addRect(630, 290, 1024, 120)
+           .setTextFont("30pt Font4")
+           .setTextAlign("center")
+           .setColor("#ffffff")
+           .addText(`#${member.user.discriminator}`, 825, 365)          
+           .setTextFont("30pt Font4")
+           .setTextAlign("center")
+           .setColor("#ffffff")
+           .addText(`${member.guild.memberCount}`, 125, 480)
+           .addText(`${member.displayName}`, 760, 155)
+           .setColor("#1A86CD")
+           .setShadowColor("rgba(22, 22, 22, 1)") // This is a nice colour for a shadow.
+           .setShadowOffsetY(5) // Drop the shadow by 5 pixels.
+           .setShadowBlur(10) 
+           .addCircle(250, 250, 177)
+           .addCircularImage(avatar, 250, 250, 170)
+           .toBuffer();
+      }
+      let Channelz = member.guild.channels.get("733961673194340415");
+      Channelz.send(`**\`Welcome\`** ${member.user}`,{
+        
+    files: [{
+      
+      attachment: await createCanvas(),
+      name:'Welcome.png'
+      
+    }]
+        
+        
+      })
+
+});
+
+
 //-----------------------------------------------------------------------------------------------------------------------------  eraser
 
 bot.on("guildMemberAdd", async member => {
 
       let BG = db.get(`${member.guild.id}.Config.Welcome.Background`);
-      if (!BG) BG = "https://cdn.discordapp.com/attachments/733961963771527169/734672045652181102/122323423423424.jpg";
+      if (!BG) BG = "https://cdn.discordapp.com/attachments/707786216300347502/734692028415213568/3242343242342432.jpg";
 
       let MSG = db.get(`${member.guild.id}.Config.Welcome.Message`);
       if (!MSG) MSG = "selamat datang bro";
@@ -356,7 +414,7 @@ bot.on("guildMemberAdd", async member => {
 bot.on("guildMemberRemove", async member => {
 
       let BG = db.get(`${member.guild.id}.Config.Welcome.Background`);
-      if (!BG) BG = "https://cdn.discordapp.com/attachments/733961963771527169/734672045652181102/122323423423424.jpg";
+      if (!BG) BG = "https://cdn.discordapp.com/attachments/707786216300347502/734692034690023474/2332423424243242432.jpg";
 
       let MSG = db.get(`${member.guild.id}.Config.Welcome.Message`);
       if (!MSG) MSG = "selamat datang bro";
